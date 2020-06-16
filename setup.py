eceab0ff362ac7bbf8524e0a3e3b6c724b0b1ce3
ascooper57 <cooper@pobox.com>
2020-06-16 13:23:33 -0700

    Published

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="stimson-web-scraper",
-    version="0.0.10",
+    version="0.0.11",
     author="Alan S. Cooper",
     author_email="cooper@pobox.com",
     description="website article / adobe pdf file discovery & extraction",
